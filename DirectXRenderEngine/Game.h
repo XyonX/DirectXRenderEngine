d08cb1be51db03aa47a96593306182af0510aa8a
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2023-09-01 23:27:57 +0530

    added Game.h class

---
+++ DirectXRenderEngine/Game.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <wrl.h>
+#include "windows.graphics.directx.h" 
+
+using namespace Microsoft::WRL;
+using namespace Windows::UI::Core;
+
+class CGame
+{
+
+public:
+	ComPtr<ID3D11Device1>device;					// the device interface - virtual reoresentation of the video adapter
+	ComPtr<ID3D11DeviceContext1>deviceContext;		// the control panel for the gpu
+	ComPtr<IDXGISwapChain1>swapChain;				// the swap chain interface
+
+	void Initialize();	//starting up code
+	void Update();		//Code that manipulate game
+	void Render();		//Draw graphics
+};
